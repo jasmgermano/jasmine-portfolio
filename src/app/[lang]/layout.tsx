03520cee9c7f5d93571f999304ba286df81377fa
jasmine <jasmigermano@gmail.com>
2024-08-09 11:40:39 -0300

    titulo e favicon

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -4,11 +4,18 @@ import { Poppins } from 'next/font/google';
 import '@/styles/globals.css';
 import { i18nConfig } from '@/config/i18n.config';
 import { ThemeProvider } from '@/context/theme-context';
+import { Metadata } from 'next';
+import Head from 'next/head';
 
 const poppins = Poppins({
   subsets: ['latin'],
   weight: ['300', '400', '500', '600', '700', '800'],
 });
+
+export const metadata: Metadata = {
+  title: 'Jasmine Germano',
+  description: 'Personal website of Jasmine Germano'
+};
 
 export function generateStaticParams() {
   const languages = i18nConfig.locales.map((lang) => ({ lang }));
@@ -18,6 +25,9 @@ export function generateStaticParams() {
 export default function RootLayout({ children, params }: { children: React.ReactNode; params: { lang: string } }) {
   return (
     <html lang={params.lang}>
+        <Head>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
         <ThemeProvider>
           <body className={poppins.className}>
             {children}
